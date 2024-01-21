d11f78bd204f17e57d7d597e71b21223ae665865
syarifsakur <107300773+syarifsakur@users.noreply.github.com>
2024-01-21 22:54:26 +0800

    second commit

--- resources/views/admindashboard.blade.php
+++ resources/views/admindashboard.blade.php
@@ -15,7 +15,6 @@
           <th>Kategori</th>
           <th>Deskripsi</th>
           <th>Jumlah</th>
-          <th>file Buku</th>
           <th>Aksi</th>
         </tr>
       </thead>
@@ -26,7 +25,6 @@
             <td>{{ $listBuku['kategori'] }}</td>
             <td>{{ $listBuku['deskripsi'] }}</td>
             <td>{{ $listBuku['jumlah'] }}</td>
-            <td><img src="{{ asset('storage/pdfs/'. $listBuku['file']) }}" alt=""></td>
             <td><button type="button" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M3 21v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15q.4 0 .775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM17.6 7.8L19 6.4L17.6 5l-1.4 1.4z"/></svg></button>
               <button type="button" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="white" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"/></svg></button></td>
           </tr>
